ebed063ce06d175ccd17c22895fb5cbe14e84c97
AtharvaKale <atharvakale1857@gmail.com>
2020-07-19 15:58:13 +0530

    added all getter methods

--- INIParser.py
+++ INIParser.py
@@ -4,52 +4,59 @@ import sys
 
 
 class INIParser:
-	'''
-	INIParser v1.0alpha072020
-	Parse, modify .ini files in python.
-	'''
 	def __init__(self, filename):
 		self.ini = open(filename, 'r').read().split('\n')
 
-		self.__headers = []
+		self.__sections = []
 		self.__settings = dict()
 		
-		self.__remove_blanks()
-		self.__get_heads()
+		self.__remove_blanks_and_comments()
+		self.__get_sects()
 		self.__get_settings()
-		#print(self.ini)
+		# print(self.ini)
 
-	def __remove_blanks(self):
+	def __remove_blanks_and_comments(self):
 		ini_t = self.ini[:]
 		self.ini.clear()
 		for line in ini_t:
-			if not line == '':
+			if not line == '' and not line.lstrip()[0] == ";":
 				self.ini.append(line)
 
-	def __get_heads(self):
+	def __get_sects(self):
 		for line in self.ini:
 			if re.match(r"\[[a-zA-Z0-9\:_\-]+\]", line):
-				self.__headers.append(re.sub(r"[\[]{1}|[\]]{1}", "", line))
+				self.__sections.append(re.sub(r"[\[]{1}|[\]]{1}", "", line))
 
 	def __get_settings(self):
-		for h in self.__headers:
-			self.__settings[h] = []
+		for line in self.ini:
+			if re.match(r"\[[a-zA-Z0-9\:_\-]+\]", line):
+				head = re.sub(r"[\[]{1}|[\]]{1}", "", line)
+				self.__settings[head] = dict()
+			else:
+				_key = line.split("=", 1)[0]
+				_val = line.split("=", 1)[1]
+				self.__settings[head][_key] = _val
+				# self.__settings[head].append(line)
 
-		print(self.__settings)
+	def get_sections(self):
+		return self.__sections
 
-	def get_headers(self):
-		return self.__headers
+	def get_all(self):
+		return self.__settings
+
+	def get(self, section, key=None):
+		if key == None:
+			return self.__settings[section]
+		else:
+			return self.__settings[section][key]
 
 	def test(self):
-		dct = {"section1": [1, 2, 3], "section2": [4, 5, 6]}
-		dct["section1"].append(4)
-		print(dct)
+		pass
 
 
 def run():
 	ini = INIParser('app.ini')
-	#print(ini.get_headers())
-#	ini.test()
+	print(ini.get("host:warlax-co"))
 
 
 if __name__ == "__main__":
