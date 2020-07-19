b546b19202e8262378ed42f531606df009258292
AtharvaKale <atharvakale1857@gmail.com>
2020-07-19 20:22:06 +0530

    updated in-code doc

--- INIParser.py
+++ INIParser.py
@@ -3,6 +3,16 @@ import re
 import sys
 import json
 
+# TODO
+#   1.
+#     method > to_json$public
+#     params > filename:str
+#	  returns > None
+#   2.
+#     method > to_yml$public
+#     params > filename:str
+#     returns > None
+
 
 class SectionNotFoundError(Exception):
 	pass
@@ -16,33 +26,105 @@ class SectionExistsError(Exception):
 	pass
 
 
+class NoValueError(Exception):
+	pass
+
+
 class INIParser:
-	def __init__(self, filename):
+	def __init__(self, filename, backup=False):
+		'''
+		INIParser object initialization method
+
+		method > __init__$private
+		params >
+			filename:str - Name of the ini file
+			backup:bool=False - set True to take backup of the file
+		returns >
+			None
+		raises >
+			None
+		'''
 		self.__file = filename
-		self.ini = open(self.__file, 'r').read().split('\n')
+		if backup:
+			os.system(f"cp {self.__file} {self.__file}-bak")
+		self.__ini = open(self.__file, 'r').read().split('\n')
 
 		self.__sections = []
 		self.__settings = {}
+		self.__comments = {}
 		
+		self.__persist_comments()
 		self.__remove_blanks_and_comments()
 		self.__get_sects()
 		self.__get_settings()
 
 	# PRIVATE FUNCTIONAL METHODS
+	def __persist_comments(self):
+		'''
+		Comment persistence method. Keeps the comments in ini file safe
+		method > 
+			__persist_comments$private
+		params >
+			None
+		returns >
+			None
+		raises >
+			None
+		'''
+		lcount = 0
+		for line in self.__ini:
+			lcount += 1
+			if not line == '':
+				if line.lstrip()[0] == ";" or line.lstrip()[0] == "#":
+					self.__comments[lcount] = line
+
 	def __remove_blanks_and_comments(self):
-		ini_t = self.ini[:]
-		self.ini.clear()
+		'''
+		Remove blank lines and comments ['#', ';'] from the ini file
+		method >
+			__remove_blanks_and_comments$private
+		params >
+			None
+		returns >
+			None
+		raises >
+			None
+		'''
+		ini_t = self.__ini[:]
+		self.__ini.clear()
 		for line in ini_t:
 			if not line == '' and not line.lstrip()[0] == ";" and not line.lstrip()[0] == "#":
-				self.ini.append(line)
+				self.__ini.append(line)
 
 	def __get_sects(self):
-		for line in self.ini:
+		'''
+		Reads and stores ini file sections in sections list
+		method >
+			__get_sects$private
+		params >
+			None
+		returns >
+			None
+		raises >
+			None
+		'''
+		for line in self.__ini:
 			if re.match(r"\[[a-zA-Z0-9\:_\-]+\]", line):
 				self.__sections.append(re.sub(r"[\[]{1}|[\]]{1}", "", line))
 
 	def __get_settings(self):
-		for line in self.ini:
+		'''
+		Reads and stores ini settings respective to the sections in dictionary
+		method >
+			__get_settings$private
+		params >
+			None
+		returns >
+			None
+		raises >
+			None
+		'''
+		for line in self.__ini:
 			if re.match(r"\[[a-zA-Z0-9\:_\-]+\]", line):
 				head = re.sub(r"[\[]{1}|[\]]{1}", "", line)
 				self.__settings[head] = dict()
@@ -50,28 +132,87 @@ class INIParser:
 				_key = line.split("=", 1)[0]
 				_val = line.split("=", 1)[1]
 				self.__settings[head][_key] = _val
-				# self.__settings[head].append(line)	
 
 	# GETTER METHODS
 	def get_sections(self):
+		'''
+		Get the list of sections available in an ini file
+		method >
+			get_sections$public
+		params >
+			None
+		return >
+			sections:list - list of sections in ini file
+		raises >
+			None
+		'''
 		return self.__sections
 
 	def get_all(self):
+		'''
+		Get all the section-wise settings at one place
+		method >
+			get_all$public
+		params >
+			None
+		returns >
+			settings:dict - Dictionary of dictionary of the settings, listed section-wise
+		raises >
+			None
+		'''
 		return self.__settings
 
 	def get(self, section, key=None):
+		'''
+		Get the value of section or key in the ini file as per requirement.
+		method >
+			get$public
+		params >
+			section:str - name of the section in ini file to get values of
+			key:str=None - name of the key under a section to get value of
+		returns >
+			section:dict - Dictionary of key value pairs under a section
+			value:str - value of the asked key under the section
+		raises >
+			SectionNotFoundError - if the param section is not present in the ini file
+			KeyNotFoundError - if the param key is nt present under the designated sections
+		'''
 		if key == None:
-			return self.__settings[section]
+			if not section in self.__sections:
+				raise SectionNotFoundError(f"The section {section} does not exist in the configuration!")
+			else:
+				return self.__settings[section]
 		else:
-			return self.__settings[section][key]
+			try:
+				return self.__settings[section][key]
+			except KeyError:
+				raise KeyNotFoundError(f"The key {key} does not exist in the section {section}! Operation failed.")
+
 
 	# SETTER METHOD
 	def set(self, section, key=None, value=None):
+		'''
+		Set the value for a particular key in a section.
+		Allows overriding of the existing keys also.
+		method >
+			set$public
+		params >
+			section:str - name of the section to search value under
+			key:str=None - name of the key under section to set value of
+			param:obj=None - value of the key
+		returns >
+			None
+		raises >
+			SectionExistsError - if the section already exists and not key, value are provided
+			NoValueError - if the key is given but not value
+		'''
 		if key == None and value == None and not section in self.__sections:
 			self.__settings[section] = dict()
 		elif key == None and value == None and section in self.__sections:
 			raise SectionExistsError(f"The {section} already exists! Provide key and value pair to set.")
 		else:
+			if value == None:
+				raise NoValueError(f"There is no value available for key {key} to set! Enter one.")
 			try:
 				self.__settings[section][key] = value
 			except KeyError:
@@ -80,6 +221,19 @@ class INIParser:
 
 	# DELETE METHOD
 	def delete(self, section, key=None):
+		'''
+		Delete the section or key under a section
+		method >
+			delete$public
+		params >
+			section:str - name of the section to delete. deletes whole section if not key is provided
+			key:str=None - name of the key under a section to delete
+		returns >
+			None
+		raises >
+			SectionNotFoundError - if param section does not exist to delete
+			KeyNotFoundError - if param key is not available under a section to delete
+		'''
 		if key == None and not section in self.__sections:
 			raise SectionNotFoundError(f"The {section} does not exist! Operation failed!")
 		elif key == None and section in self.__sections:
@@ -91,26 +245,78 @@ class INIParser:
 				raise KeyNotFoundError(f"The key {key} in section {section} does not exist! Operation failed!")
 
 	# COMMIT METHOD
-	def commit(self):
-		write_file = open("up_" + self.__file, "a+")
-		write_file.truncate(0)
+	def __pre_commit(self):
+		'''
+		Pre commit method to write changes in internal ini structure
+		Compiles sections, key-value pairs and comments in internal ini structure
+		method >
+			__pre_commit$private
+		params >
+			None
+		returns >
+			None
+		raises >
+			None
+		'''
+		self.__ini.clear()
 		for sect in self.__settings.keys():
-			write_file.write("[" + sect + "]\n")
+			self.__ini.append("[" + str(sect) + "]\n")
 			for k, v in self.__settings[sect].items():
-				write_file.write(k + "=" + v + "\n")
-			write_file.write("\n")
+				self.__ini.append(str(k) + "=" + str(v) + "\n")
+			self.__ini.append("\n")
+	
+		for line_num, cmnt in self.__comments.items():
+			self.__ini.insert(line_num - 1, cmnt + "\n")
+
+	def commit(self):
+		'''
+		The final commit method to write internal ini structure in the file.
+		Truncates the existing file and writes compiled configuration to it
+		method >
+			commit$public
+		params >
+			None
+		returns >
+			None
+		raises >
+			None
+		'''
+		write_file = open(self.__file, "a+")
+		write_file.truncate(0)
+		self.__pre_commit()
+		for line in self.__ini:
+			write_file.write(line)
+		write_file.close()
 
 
 def run():
+	'''
+	Runner method
+	'''
+	# Initiate INIParser
 	ini = INIParser('app.ini')
-	print(ini.get("host:warlax-co", "logger"))
+
+	# Get a section
+	print(ini.get("host:warlax-co"))
+
+	# Create a blank section
+	ini.set("host:agsft-com")
+
+	# Set value under new section
+	ini.set("host:agsft-com", "server_count", 8)
+
+	# Write value to new section directly
 	ini.set("host:groots-in", "username", "grootsadmin")
 	ini.set("host:groots-in", "password", "groots@123")
-	ini.set("host:groots-in", "website", "www.groots.in")
+
+	ini.set("baseconfig", "superuser", "dexter")
+	
+	# Delete certain key
 	ini.delete("baseconfig", "port")
-	print(ini.get_all())
+
+	# Commit the changes to the file
 	ini.commit()
 
 
 if __name__ == "__main__":
-	run()
+	# run()
